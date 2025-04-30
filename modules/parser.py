31829bb71737091ccb5d946db1ee5af049643249
davi-magic <davischuinanunes@gmail.com>
2025-04-30 14:23:13 -0300

    Update parser.py

--- modules/parser.py
+++ modules/parser.py
@@ -1,2 +1,9 @@
-def parse_data(text):
-    return {"texto": text}
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+def extract_tables_from_challenge_place(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    tables = pd.read_html(str(soup))
+    return tables
