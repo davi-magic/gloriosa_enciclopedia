777e40729fd39c5fc810536b5f50c67bedc64c15
davi-magic <davischuinanunes@gmail.com>
2025-04-30 23:16:59 -0300

    Delete modules directory

--- modules/parser.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-
-def extract_tables_from_challenge_place(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    tables = pd.read_html(str(soup))
-    return tables
