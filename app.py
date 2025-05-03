c36e185c2593bcc489e78c16c599236285d733b7
davi-magic <davischuinanunes@gmail.com>
2025-05-03 16:06:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-# app.py
-# Código principal da aplicação
+import streamlit as st
+from modules.interface import mostrar_interface
+
+st.set_page_config(page_title="A Gloriosa Enciclopédia", layout="wide")
+
+def main():
+    st.markdown(
+        "<h1 style='text-align: center; color: gold;'>A Gloriosa Enciclopédia</h1>",
+        unsafe_allow_html=True
+    )
+    mostrar_interface()
+
+if __name__ == "__main__":
+    main()
